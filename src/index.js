2b38f95991bc7b59460394db7f9bb797a9964c21
Avishkar Pawar <avishkarp644@gmail.com>
2025-06-06 01:07:44 +0530

    User Model Created

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8500,() => {
+        console.log(`Server is running at port :${process.env,PORT}`);
+    })
+
+    app.on((error)  => {
+        console.log('server failed to start' , error);
+    })
+})
+.catch((error) =>{
+    console.log("MongoDB connection failed", error);
+} )
